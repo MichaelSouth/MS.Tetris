a5750b647c2fc735b7ca7a9dd483e85668f08a6a
Michael South <michaelsouth@hotmail.co.uk>
2020-11-21 18:25:46 +0000

    Add swagger.

--- Startup.cs
+++ Startup.cs
@@ -10,12 +10,24 @@ namespace MS.Tetris
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseDefaultFiles();
             app.UseStaticFiles(); // For the wwwroot folder
 
